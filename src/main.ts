5599a419f7f4f3b5b8998b203c1b786e518928b7
Maulidin Zakaria <maulidinzakaria123@gmail.com>
2024-11-17 17:37:37 +0700

    add: enable CORS in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*', // Untuk sementara, Anda bisa mengizinkan semua asal. Namun, disarankan menggunakan domain tertentu untuk produksi.
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   // Winston global logger
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
